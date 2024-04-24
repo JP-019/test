7abd9344325d4096486f7cbfe2b583587cf94aa9
Julio <juliocgp30@gmail.com>
2024-04-24 17:33:06 -0600

    proyecto base limpio

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'home.dart'; // Importa el archivo home.dart
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    const title = 'MAIN';
+    return MaterialApp(
+      title: title,
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: HomePage(), // Usa el widget HomePage como la página de inicio
+    );
+  }
+}
